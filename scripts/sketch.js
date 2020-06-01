b9f2bdf7504a2e120f441d50953c490112d1fe0a
Samuel Spiers <sammyspiers@yahoo.co.uk>
2020-06-01 01:31:02 +0100

    Created moving particle

---
+++ scripts/sketch.js
@@ -0,0 +1,30 @@
+let p; 
+
+function setup() {
+	createCanvas(window.innerWidth, window.innerHeight);
+	p = new Particle();
+}
+
+function draw() {
+	p.draw();
+	p.update(); 
+}
+
+class Particle {
+
+	constructor() {
+		this.pos = createVector(random(width), random(height));
+		this.vel = createVector(random(-2, 2), random(-2, 2));
+		this.size = 10;
+	}
+
+	update() {
+		this.pos.add(this.vel);
+	}
+
+	draw() {
+		noStroke();
+		fill('#61B72C');
+		circle(this.pos.x, this.pos.y, this.size);
+	}
+}
