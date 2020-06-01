4005e276ce925c42b10b63f9e6e0ba5c7fff8061
Samuel Spiers <sammyspiers@yahoo.co.uk>
2020-06-01 01:07:15 +0100

    Playing around with p5

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,22 +1,57 @@
+const numDots = 100
+const colours = ["#ff3434", "#ffe23c", "#5fd8ff"]
+let dots = []
+let clusters = []
+
 function setup() {
+	initDots()
+	initClusters()
 	createCanvas(windowWidth, windowHeight)
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
 
 function draw() {
-	noStroke()
-	fill(color(255, 204, 0)) 
-	const c = circle(500, 500, 50)
-	noStroke()
-	fill(color('magenta')) 
-	const c2 = circle(400, 500, 50)
+	for (dot of dots) {
+		drawDot(dot.x, dot.y, dot.colour)
+	}
 }
 
-function drawRandomCircle() {
+function drawDot(x, y, colour) {
 	noStroke()
-	fill(color(255, 204, 0)) 
-	const c = circle(500, 500, 50)
+	fill(color(colour))
+	circle(x, y, 25)
+}
+
+function animateDotsToCluster() {
+	trans
+}
+
+function initClusters() {
+	for (let i = 0; i < colours.length; i++) {
+		clusters.push({
+			x: randomNumber(windowWidth), 
+			y: randomNumber(windowHeight)
+		});
+	}
+}
+
+function initDots() {
+	for (let i = 0; i < numDots; i++) {
+		dots.push({
+			x: randomNumber(windowWidth), 
+			y: randomNumber(windowHeight),
+			colour: randomColour()
+		});
+	}
+}
+
+function randomNumber(maxValue) {
+	return Math.floor(Math.random() * maxValue)
+}
+
+function randomColour() {
+	return colours[Math.floor(Math.random() * colours.length)]
 }
