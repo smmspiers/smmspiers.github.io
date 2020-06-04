88d4d86369b8efb62ef243b4cb467545c3deb1c0
Samuel Spiers <sammyspiers@yahoo.co.uk>
2020-06-04 16:01:21 +0100

    Number of particles now dependent on page width

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,59 +1,60 @@
-let particles = []; 
+let particles = [];
 
 function setup() {
-	createCanvas(window.innerWidth, window.innerHeight);
-	const nParticles = Math.floor(window.innerHeight / 20);
-	for (let i = 0; i < nParticles; i++) {
-		particles.push(new Particle());
-	}
-	strokeWeight(0.2);
+    createCanvas(window.innerWidth, window.innerHeight);
+    const nParticles = Math.floor(window.innerWidth / 35);
+    console.log(nParticles);
+    for (let i = 0; i < nParticles; i++) {
+        particles.push(new Particle());
+    }
+    strokeWeight(0.2);
 }
 
 function draw() {
-	background('#FFFFFF');
-	particles.forEach(p => {
-		p.update();
-		p.draw();
-		p.connect(particles);
-	});
+    background('#FFFFFF');
+    particles.forEach(p => {
+        p.update();
+        p.draw();
+        p.connect(particles);
+    });
 }
 
 class Particle {
 
-	constructor() {
-		this.pos = createVector(random(width), random(height));
-		this.vel = createVector(random(-1, 1), random(-1, 1));
-		this.size = 5;
-	}
+    constructor() {
+        this.pos = createVector(random(width), random(height));
+        this.vel = createVector(random(-1, 1), random(-1, 1));
+        this.size = 5;
+    }
 
-	update() {
-		this.pos.add(this.vel);
-		this.edges();
-	}
+    update() {
+        this.pos.add(this.vel);
+        this.edges();
+    }
 
-	draw() {
-		noStroke();
-		fill('#61B72C');
-		circle(this.pos.x, this.pos.y, this.size);
-	}
+    draw() {
+        noStroke();
+        fill('#61B72C');
+        circle(this.pos.x, this.pos.y, this.size);
+    }
 
-	edges() {
-		if (this.pos.x < 0 || this.pos.x > width) {
-			this.vel.x *= -1
-		}
+    edges() {
+        if (this.pos.x < 0 || this.pos.x > width) {
+            this.vel.x *= -1
+        }
 
-		if (this.pos.y < 0 || this.pos.y > height) {
-			this.vel.y *= -1
-		}
-	}
+        if (this.pos.y < 0 || this.pos.y > height) {
+            this.vel.y *= -1
+        }
+    }
 
-	connect(particles) {
-		particles.forEach(p => {
-			const d = dist(this.pos.x, this.pos.y, p.pos.x, p.pos.y);
-			if (d < 125) {
-				stroke('#61B72C');
-				line(this.pos.x, this.pos.y, p.pos.x, p.pos.y);
-			}
-		});
-	}
+    connect(particles) {
+        particles.forEach(p => {
+            const d = dist(this.pos.x, this.pos.y, p.pos.x, p.pos.y);
+            if (d < 125) {
+                stroke('#61B72C');
+                line(this.pos.x, this.pos.y, p.pos.x, p.pos.y);
+            }
+        });
+    }
 }
